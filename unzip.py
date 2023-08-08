371034ee8df20a8ab4c054685b38fcc3eb0c1f74
David <david.holt2002@gmail.com>
2023-08-08 12:26:52 +0800

    get both document and xml content

--- unzip.py
+++ unzip.py
@@ -1,15 +1,32 @@
-from zipfile import ZipFile
+import zipfile
+# from lxml import etree
+# import pandas as pd
 
-def unzipfile(filename):
-    """Unzips docx files and store in current directory"""
-    # with ZipFile(filename, 'r') as doc_file:
-    #     doc_file.extractall(path="C:\Users\david\OneDrive\Desktop\Uni\,Third Year Sem 2\Professional Computing\Project\CITS3200_Project")
-    # doc_file.close()
-    print("hi")
-    with open(filename) as f:
-        zip = ZipFile(f)
-        print(zip)
-        xml_content = zip.read('qwerty.txt')
-    print(xml_content)
+FILE_PATH = 'C:/Users/david/OneDrive/Desktop/Uni/,Third Year Sem 2/Professional Computing/Project/CITS3200_Project'
 
-unzipfile("C:/Users/david/OneDrive/Desktop/Uni/,Third Year Sem 2/Professional Computing/Project/CITS3200_Project/yellow.zip")
+class Application():
+    def __init__(self):
+        self.document_content = self.get_xml(f'{FILE_PATH}/hi.docx', 0)
+        self.settings_content = self.get_xml(f'{FILE_PATH}/hi.docx', 1)
+        # xml_tree = self.get_xml_tree(xml_content)
+
+    def get_xml(self, docx_filename, content):
+        with open(docx_filename, "rb") as f:
+            zip = zipfile.ZipFile(f)
+            if content == 0:
+                xml_content = zip.read('word/document.xml')
+            else:
+                xml_content = zip.read('word/settings.xml')
+        return xml_content
+
+    def document_content(self):
+        return self.document_content
+    
+    def settings_content(self):
+        return self.settings_content
+
+    # def get_xml_tree(self, xml_string):
+    #     return (etree.fromstring(xml_string))
+
+a = Application()
+print(a.settings_content)
