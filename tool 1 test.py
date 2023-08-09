d32ae5d949b76ca023415e994c4415e41927252c
SeanLixu23445997 <23445997@student.uwa.edu.au>
2023-08-09 14:13:47 +0800

    adding resources

--- tool 1 test.py
+++ tool 1 test.py
@@ -1,20 +1,34 @@
 import csv
 import shutil
+import os
 
 class main:
 
-
+    ## initialising file as object
     def __init__(self, path):
         try:
             with open(path) as file:
                 self.openFile = csv.reader(file)
+            self.filePath = path
             print("Success")
+            self.duplicate('copyCSV.txt')
         except Exception as e:
             print("path didnt work: {e}")
 
-    def duplicate(src, dst):
+    def duplicate(self, dst):
+        try:
+            shutil.copy(self.filePath, dst)
+            print("copy success")
+        except:
+            print("could not copy file")
+
+
         
 
-            
-
 test = main('example LMS test log.csv')
+
+
+print(os.getcwd())
+
+
+
