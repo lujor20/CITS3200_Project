122bd15c54407ccec356307ab432ae75dc6290c7
David <david.holt2002@gmail.com>
2023-08-10 12:45:52 +0800

    removed unnecessary code

--- unzip.py
+++ unzip.py
@@ -2,18 +2,18 @@ import zipfile
 # from lxml import etree
 # import pandas as pd
 
-FILE_PATH = 'C:/Users/david/OneDrive/Desktop/Uni/,Third Year Sem 2/Professional Computing/Project/CITS3200_Project'
-
-class Application():
-    def __init__(self):
-        self.document_content = self.get_xml(f'{FILE_PATH}/hi.docx', 0)
-        self.settings_content = self.get_xml(f'{FILE_PATH}/hi.docx', 1)
+class Extract():
+    def __init__(self, filename):
+        """Find and store xml content"""
+        self.document_content = self.get_xml(filename, "document")
+        self.settings_content = self.get_xml(filename, "settings")
         # xml_tree = self.get_xml_tree(xml_content)
 
-    def get_xml(self, docx_filename, content):
+    def get_xml(self, docx_filename, type):
+        """Get the contents of relevant xml files"""
         with open(docx_filename, "rb") as f:
             zip = zipfile.ZipFile(f)
-            if content == 0:
+            if type == "document":
                 xml_content = zip.read('word/document.xml')
             else:
                 xml_content = zip.read('word/settings.xml')
@@ -22,5 +22,5 @@ class Application():
     # def get_xml_tree(self, xml_string):
     #     return (etree.fromstring(xml_string))
 
-a = Application()
-print(a.document_content)
+xml = Extract("hi.docx")
+print(xml.document_content)
