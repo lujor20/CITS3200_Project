b413ba7285cc7026b0219a32f7d977e262012d00
William Salim <23291312@student.uwa.edu.au>
2023-08-13 01:20:44 +0800

    fix rsid.py file

--- rsid.py
+++ rsid.py
@@ -1,13 +1,7 @@
 import os
-import re
-import json
-import shutil
-from subprocess import PIPE, run
 import sys
 from zipfile import ZipFile
 from bs4 import BeautifulSoup
-import xml.etree.ElementTree as ET
-from xml.dom import minidom
 
 def main(sourcefile):
     cwd = os.getcwd()
@@ -31,69 +25,23 @@ def main(sourcefile):
                 full_path_documentxml = os.path.join(root, filename)
                 print(f'{full_path_documentxml}')
 
-                # tree = ET.parse(full_path_documentxml)
-                # root = tree.getroot()
-
-                # print(root.findall(".//t"))
-                # for i in range(3):
-                #     print(root[0][0][i][1].text)
-
-                # for child in myroot:
-                #     print(child.tag, child.attrib)
-
-                # for r in root.iter('r'):
-                #     print(r.text)
-
-                # for text in myroot.findall('w:r'):
-                #     t = text.find('w:t').text
-                #     print(t)
-
-                # Textpattern = r'<w:t>.*</w:t>'
-                # rsidPattern = r'w:rsidR="(.{8})"'
-
-                # with open(full_path_documentxml, 'r') as file:
-                #     content = file.read()
-                #     matches = re.findall(Textpattern, content)
-                #     for match in matches:
-                #         print(f"{match}")
-                    
-                #     rsidMatches = re.findall(rsidPattern, content)
-                #     for rsidMatch in rsidMatches:
-                #         print(f"{rsidMatch}")
-
-
                 with open(full_path_documentxml, 'r') as fp:
                     soup = BeautifulSoup(fp, 'xml')
                 
-                # print(soup.get_text())
-                # print(soup.find_all('r'))
-                p_tag = soup.r
-
-                print(p_tag.contents[1])
-
-                # # Reading the data inside the xml file to a variable under the name  data
-                # with open(full_path_documentxml, 'r') as f:
-                #     data = f.read() 
-
-                # # Passing the stored data inside the beautifulsoup parser 
-                # bs_data = BeautifulSoup(data, 'xml') 
-
-                # # Finding all instances of tag   
-                # b_unique = bs_data.find_all('w:r w:rsidR') 
-                # print(b_unique) 
-
-                # # Using find() to extract attributes of the first instance of the tag 
-                # b_name = bs_data.find('w:rsid', {'w:val':'00082485'}) 
-                # print(b_name) 
-
-                # # Extracting the data stored in a specific attribute of the `child` tag 
-                # value = b_name.get('qty') 
-                # print(value)
+                '''Gets the text of a particular run'''
+                default_rsidR = soup.p['w:rsidR']
+                for run in soup.find_all('r'):
+                    try:
+                        print(run.t.string)
+                        print(run['w:rsidR'])
+                        
+                    except:
+                        print(default_rsidR)
 
 if __name__=="__main__":
     args = sys.argv
     if len(args) != 2:
-        raise Exception("You must pass one argument only!")
+        raise Exception("Usage: python rsid.py <docx file path>")
     
     sourcefile = args[1]
     main(sourcefile)
