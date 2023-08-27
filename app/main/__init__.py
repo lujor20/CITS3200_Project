9c4797a6ea149f5a8eda5b5c7eb2fb96410967fe
William Salim <23291312@student.uwa.edu.au>
2023-08-27 22:56:43 +0800

    updates __init__.py to include static folder

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,3 +1,3 @@
 from flask import Blueprint
 
-main = Blueprint('main', __name__, template_folder='templates')
+main = Blueprint('main', __name__, template_folder='templates', static_folder='static', static_url_path='/main/static')
