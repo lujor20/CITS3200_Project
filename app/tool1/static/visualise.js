39ea765c2028242388a2466843935e2d7ee69d0f
lujor20 <jordonmlu@gmail.com>
2023-08-29 12:50:05 +0800

    fixed bug - find_rsid_style_sheet_index() added

--- app/tool1/static/visualise.js
+++ app/tool1/static/visualise.js
@@ -1,16 +1,28 @@
 // incredibly hacky function
+let rsid_style_stylesheet_index;
+
+function find_rsid_style_index() {
+  for (let x=0; x < document.styleSheets.length; x++) {
+    if (document.styleSheets[x].title === "rsid_css") {
+      rsid_style_stylesheet_index = x;
+      break;
+    }
+  }
+};
+
+
 function bcolour_rsid() {
   let color = document.getElementById("rsid_color").value;
   let rsid_index = document.getElementById("select_rsid").value;
   let html_stylesheet_index = 1 // defined second in the HTML, hence index 1.
-  let cssRules = document.styleSheets[html_stylesheet_index].cssRules[rsid_index];
+  let cssRules = document.styleSheets[rsid_style_stylesheet_index].cssRules[rsid_index];
 
   let expected_rsid = rsids[1];
   let actual_rsid = cssRules.selectorText;
   
   cssRules.style.backgroundColor = color;
 
-  console.log(expected_rsid.concat("with", actual_rsid, color))
+  console.log(expected_rsid.concat("with", actual_rsid, color));
 };
 
 function populate_select() {
@@ -24,6 +36,10 @@ function populate_select() {
   }
 }
 
+function onload_events() {
+  populate_select();
+  find_rsid_style_index();
+}
 
 
 
